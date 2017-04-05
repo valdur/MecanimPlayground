61c3351407bfebacc0e371f5f69e7a657124490c
Rafał Kańka <ainaruth@gmail.com>
2017-04-05 12:59:50 +0200

    stuff

---
+++ Assets/_Code/CharacterHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour {
+
+    Animator mecanim;
+    bool dead;
+
+    // Use this for initialization
+    void Start() {
+        mecanim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.O)) {
+            mecanim.SetTrigger("Hit");
+        }
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+            dead = !dead;
+            mecanim.SetBool("Dead", dead);
+        }
+    }
+}
