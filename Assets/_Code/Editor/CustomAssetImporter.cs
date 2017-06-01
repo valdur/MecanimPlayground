f14d14ed92cd9abbef4e676cd218a5def641ff82
Rafał Kańka <ainaruth@gmail.com>
2017-06-01 17:19:59 +0200

    stuff

---
+++ Assets/_Code/Editor/CustomAssetImporter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CustomAssetImporter : AssetPostprocessor {
+    void OnPreprocessModel() {
+        var modelImporter = assetImporter as ModelImporter;
+        modelImporter.importMaterials = false;
+        //modelImporter.animationType = ModelImporterAnimationType.None;
+    }
+}
